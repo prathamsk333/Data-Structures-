a654938522e07cfe7f5ff15bc8167c38ba8fcac2
Pratham S Kore <pratham.sk333@gmail.com>
2024-05-21 15:25:36 +0530

    Add files via upload

---
+++ Binary_Tree-preorder&inorder.cpp
@@ -0,0 +1,11 @@
+The space complexity is 
+�
+(
+�
+)
+O(n) due to the recursion stack (in the worst case, the tree is skewed). Additionally, storing the resulting tree also requires 
+�
+(
+�
+)
+O(n) space.
